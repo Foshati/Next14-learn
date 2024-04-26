b7694372114fedcb68b9cba6ab0e6589d800a71b
Foshati <amirrezafoshati@gmail.com>
2024-04-26 16:03:14 +0330

    usePathname and used in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,23 +1,43 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const PathName = usePathname();
+
   return (
     <div className="m-4">
       <ul className="flex gap-8 text-lg">
         <li>
           {/* <a href="/">home</a> */}
-          <Link href="/">home</Link>
+          <Link href="/" className={PathName == "/" ? "active" : ""}>
+            home
+          </Link>
         </li>
         <li>
-          <Link href="/blog">blog</Link>
+          <Link href="/blog" className={PathName == "/blog" ? "active" : ""}>
+            blog
+          </Link>
         </li>
         <li>
-          <Link href="/dashboard">dashboard</Link>
+          <Link
+            href="/dashboard"
+            className={PathName == "/dashboard" ? "active" : ""}
+          >
+            dashboard
+          </Link>
         </li>
         <li>
-          <Link href="/dashboard/settings">settings</Link>
+          <Link
+            href="/dashboard/settings"
+            className={PathName == "/dashboard/settings" ? "active" : ""}
+          >
+            settings
+          </Link>
         </li>
       </ul>
+      {/* <p>{PathName}</p> */}
     </div>
   );
 };
