8f59244bb23529426d6c11539a94f5a60796a174
Foshati <amirrezafoshati@gmail.com>
2024-04-30 22:00:18 +0330

    css module in next.js

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,3 +1,5 @@
+import styles from "./posts.module.css";
+
 const getPost = async () => {
   const data = await fetch("https://jsonplaceholder.typicode.com/posts");
   const res = await data.json();
@@ -6,19 +8,18 @@ const getPost = async () => {
 };
 
 export default async function Posts() {
-  if (parseInt(params.id) == 2) {
-    notFound();
-  }
+  // // notFound()
+  // if (parseInt(params.id) == 2) {
+  //   notFound();
+  // }
   const Posts = await getPost();
   //   console.log(Posts);
-
-  
 
   return (
     <div>
       {Posts.map((post) => (
         <ul key={post.id} className="m-4">
-          <li>{post.title}</li>
+          <li className={styles.bgColor}>{post.title}</li>
         </ul>
       ))}
     </div>
