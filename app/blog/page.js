e5c8229e65ed9e675173f95101a58fdeda895a51
Foshati <amirrezafoshati@gmail.com>
2024-05-01 19:57:02 +0330

    image in next.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,4 +1,7 @@
 import Script from "next/script";
+import Image from "next/image";
+
+import imagePlaceHolder from "@/public/images/image.png";
 
 export default function page() {
   const TextColor = {
@@ -8,11 +11,39 @@ export default function page() {
     <div>
       <p style={{ color: "blue", fontSize: "30px" }}>blog</p>
       <p style={TextColor}>blog2</p>
-
       {/* Script in next.js */}
-      <Script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js" /> {/* cdn */}
-      <Script id="inline-script">{`console.log("fa inLine")`}</Script> {/* inline */}
+      <Script
+        strategy="afterInteractive"
+        src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js"
+      />
+      {/* cdn */}
+      <Script id="inline-script">{`console.log("fa inLine")`}</Script>
+      {/* inline */}
       <Script src="/js/test.js" /> {/* external */}
+      {/* image in next */}
+      <div>
+        {/* <img src="/images/image.png" alt="image" width={300} height={300} /> */}
+
+        <Image src="/images/image.png" alt="image" width={300} height={300} />
+        <Image
+          src={imagePlaceHolder}
+          placeholder="blur"
+          alt="image"
+          width={300}
+          height={300}
+        />
+
+   {/*      <div>
+          <Image
+            src="/images/image.png"
+            placeholder="blur"
+            blurDataUrl=""
+            alt="image"
+            width={300}
+            height={300}
+          />
+        </div> */}
+      </div>
     </div>
   );
 }
