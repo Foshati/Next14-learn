86fe7563d90d68bddea2c6820e98985c86a6de33
Foshati <amirrezafoshati@gmail.com>
2024-05-08 01:53:09 +0330

    server actions-2 in next.js

--- components/CreateBook.jsx
+++ components/CreateBook.jsx
@@ -1,16 +1,40 @@
+import { revalidatePath } from "next/cache";
+
 // server action function
-const addBook = async () => {
+const addBook = async (formData) => {
   "use server";
-  console.log("test");
+  //   console.log(formData.get("title"));
+  const title = formData.get("title");
+  const body = formData.get("body");
+  const res = await fetch("http://localhost:3001/posts", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+      body: body,
+    }),
+  });
+  const data = await res.json();
+  revalidatePath("/posts");
+  return data;
 };
 
 export default function CreateBook() {
   return (
     <div className="mx-auto mt-14 card bg-base-200 w-80">
       <form action={addBook} className="card-body">
-        <input placeholder="name book" className="input input-bordered" />
         <input
+          placeholder="name book"
+          className="input input-bordered"
+          name="title"
+        />
+
+        <textarea
           placeholder="Description of the book"
+          name="body"
+          id=""
           className="input input-bordered"
         />
         <button type="submit" className="btn btn-neutral">
