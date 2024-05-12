009c72fc456d2392d281ba809dcc77a75c05b575
Foshati <amirrezafoshati@gmail.com>
2024-05-12 22:56:23 +0330

    post - request body in next.js

--- app/api/route.js
+++ app/api/route.js
@@ -8,3 +8,19 @@ import { NextResponse } from "next/server";
 export function GET(NextRequest) {
   return NextResponse.json({ message: "hello fa" }, { status: 200 });
 }
+
+
+
+
+
+
+export async function POST(request) {
+  // const body = await request.json();
+  // console.log(body);
+  // console.log(body.name);
+
+  const body = await request.formData();
+  console.log(body);
+
+  return NextResponse.json({ message: "hello fa - method post" });
+}
